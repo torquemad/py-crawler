6619439ca947376a226e969ffea529877c42ec58
Louis Moore <burntliketoast@gmail.com>
2015-10-05 16:47:03 +1100

    Blueprint of crawler app

---
+++ crawler.py
@@ -0,0 +1,31 @@
+class AppCrawler:
+	
+	def __init__(self, starting_url, depth)
+		self.starting_url = starting_url
+		self.depth = depth
+	  self.apps = []
+	def crawl(self):
+		return
+  
+  def get_data_from_link(self, link):
+    return
+
+class App:
+
+  def __init__(self, thing, thing1, thing2, links)
+    self.thing = thing
+    self.thing1 = thing1
+    self.thing2 = thing2
+    self.links = links
+    
+  def __str__(self):
+    return("Thing: " + self.thing.encode('UTF-8') +
+    "\r\nThing1: " + self.thing1.encode('UTF-8') +
+    "\r\nThing2: " + self.thing2.encode('UTF-8') + "\r\n"
+
+
+crawler = AppCrawler('www.website.com', 0)
+crawler.crawl()
+
+for app in self.apps:
+  print app
